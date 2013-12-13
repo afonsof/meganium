346e0ca94209d80e284ecaa8bffbc3cb395a9fbc
Afonso França <afonso.franca@gmail.com>
2013-12-13 01:08:12 -0200

    Início da implementação da extensão de Seleção de Fotos.

---
+++ src/MegaSite.Site/Areas/Extension/Controllers/PhotoSelectorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace MegaSite.Site.Areas.Extension.Controllers
+{
+    public class PhotoSelectorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            return Json(new[]
+            {
+                new
+                {
+                    test = 1
+                }
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ src/MegaSite.Site/Areas/Extension/ExtensionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MegaSite.Site.Areas.Extension
+{
+    public class ExtensionAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Extension";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Extension_default",
+                "Extension/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
